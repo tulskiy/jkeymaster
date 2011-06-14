ca3c1d56c3a57d630fbe9fe456b1005d734a04ce
tulskiy <tulskiy@gmail.com>
2011-06-14 22:48:32 +0600

    [windows] handle some exceptions to virtual key codes

--- src/main/java/com/tulskiy/keymaster/windows/WindowsProvider.java
+++ src/main/java/com/tulskiy/keymaster/windows/WindowsProvider.java
@@ -1,6 +1,5 @@
 package com.tulskiy.keymaster.windows;
 
-import com.tulskiy.keymaster.common.MediaKey;
 import com.tulskiy.keymaster.common.Provider;
 import org.bridj.Pointer;
 
@@ -9,24 +8,36 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
 import java.util.Collections;
-import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.Map;
 
 import static com.tulskiy.keymaster.windows.User32.*;
+import static java.awt.event.KeyEvent.*;
 
 /**
  * Author: Denis Tulskiy
  * Date: 6/12/11
  */
 public class WindowsProvider implements Provider {
-    private ActionListener mediaListener;
+    private static final Map<Integer, Integer> codeExceptions = new HashMap<Integer, Integer>() {{
+        put(VK_INSERT, 0x2D);
+        put(VK_DELETE, 0x2E);
+        put(VK_ENTER, 0x0D);
+        put(VK_COMMA, 0xBC);
+        put(VK_PERIOD, 0xBE);
+        put(VK_PLUS, 0xBB);
+        put(VK_MINUS, 0xBD);
+        put(VK_SLASH, 0xBF);
+        put(VK_SEMICOLON, 0xBA);
+    }};
+
     private static volatile int idSeq = 0;
 
     private Map<Integer, ActionListener> idToListener = Collections.synchronizedMap(new HashMap<Integer, ActionListener>());
     private Map<Integer, KeyStroke> idToKeyStroke = Collections.synchronizedMap(new HashMap<Integer, KeyStroke>());
     private final Map<KeyStroke, ActionListener> toRegister = new HashMap<KeyStroke, ActionListener>();
     private boolean listen;
+    private Boolean reset = false;
     private Thread thread;
 
     public void init() {
@@ -35,12 +46,28 @@ public class WindowsProvider implements Provider {
                 Pointer<MSG> msgPointer = Pointer.allocate(MSG.class);
                 listen = true;
                 while (listen) {
+                    synchronized (reset) {
+                        if (reset) {
+                            for (Integer id : idToListener.keySet()) {
+                                UnregisterHotKey(null, id);
+                            }
+
+                            idToListener.clear();
+                            idToKeyStroke.clear();
+                            reset = false;
+                        }
+                    }
+
                     synchronized (toRegister) {
                         for (Map.Entry<KeyStroke, ActionListener> entry : toRegister.entrySet()) {
                             int id = idSeq++;
                             KeyStroke keyCode = entry.getKey();
                             ActionListener listener = entry.getValue();
-                            if (RegisterHotKey(null, id, getModifiers(keyCode), keyCode.getKeyCode())) {
+                            int code = keyCode.getKeyCode();
+                            if (codeExceptions.containsKey(code)) {
+                                code = codeExceptions.get(code);
+                            }
+                            if (RegisterHotKey(null, id, getModifiers(keyCode), code)) {
                                 logger.info("Registering hotkey: " + keyCode);
                                 idToListener.put(id, listener);
                                 idToKeyStroke.put(id, keyCode);
@@ -77,7 +104,6 @@ public class WindowsProvider implements Provider {
     }
 
     public void registerMediaKeyListener(ActionListener listener) {
-        mediaListener = listener;
         registerMediaKeys();
     }
 
@@ -106,12 +132,7 @@ public class WindowsProvider implements Provider {
     }
 
     public void reset() {
-        for (Integer id : idToListener.keySet()) {
-            UnregisterHotKey(null, id);
-        }
-
-        idToListener.clear();
-        idToKeyStroke.clear();
+        reset = true;
     }
 
     public void stop() {
