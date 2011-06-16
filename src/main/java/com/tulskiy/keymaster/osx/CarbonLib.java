58f406187c08e773cde560902deaef29ace7692d
Denis Tulskiy <tulskiy@gmail.com>
2011-06-16 09:19:39 +0600

    [osx] tests

--- src/main/java/com/tulskiy/keymaster/osx/CarbonLib.java
+++ src/main/java/com/tulskiy/keymaster/osx/CarbonLib.java
@@ -13,6 +13,12 @@ public class CarbonLib {
         Native.register(NativeLibrary.getInstance("Carbon"));
     }
 
+    public static final OSStatus noErr = new OSStatus(0);
+    public static final OSStatus eventAlreadyPostedErr = new OSStatus(-9860);
+    public static final OSStatus eventTargetBusyErr = new OSStatus(-9861);
+    public static final OSStatus eventClassInvalidErr = new OSStatus(-9862); //Note More on page 213 - Carbon_Event_Manager_Ref.pdf
+
+
     public static native Pointer GetApplicationEventTarget();
 
     /* OSStatus InstallEventHandler(EventTargetRef inTarget, EventHandlerUPP inHandler, ItemCount inNumTypes, const EventTypeSpec* inList, void* inUserData, EventHandlerRef *outRef) */
@@ -35,7 +41,7 @@ public class CarbonLib {
 
 
     /* typedef SInt32 OSStatus */
-    public class OSStatus extends IntegerType {
+    public static class OSStatus extends IntegerType {
         public OSStatus() {
             this(0);
         }
@@ -45,13 +51,8 @@ public class CarbonLib {
         }
     }
 
-    OSStatus noErr = new OSStatus(0);
-    OSStatus eventAlreadyPostedErr = new OSStatus(-9860);
-    OSStatus eventTargetBusyErr = new OSStatus(-9861);
-    OSStatus eventClassInvalidErr = new OSStatus(-9862); //Note More on page 213 - Carbon_Event_Manager_Ref.pdf
-
     /* Don't see defined anywhere, but used where integer is expected. */
-    public class ItemCount extends IntegerType {
+    public static class ItemCount extends IntegerType {
         public ItemCount() {
             this(0);
         }
@@ -62,24 +63,24 @@ public class CarbonLib {
     }
 
     /* EventTypeSpec */ /* struct EventTypeSpec { UInt32 eventClass; UInt32 eventKind; }; typedef struct EventTypeSpec EventTypeSpec */
-    public class EventTypeSpec extends Structure {
+    public static class EventTypeSpec extends Structure {
         public int eventClass;
         public int eventKind;
     }
 
     /* EventHotKeyID: struct EventHotKeyID { OSType signature; UInt32 id; }; */
-    public class EventHotKeyID extends Structure {
+    public static class EventHotKeyID extends Structure {
         public int signature;
         public int id;
     }
 
 
-    public interface EventHandlerUPP extends EventHandlerProcPtr {
+    public static interface EventHandlerUPP extends EventHandlerProcPtr {
 
     }
 
     /* typedef OSStatus (*EventHandlerProcPtr) ( EventHandlerCallRef inHandlerCallRef, EventRef inEvent, void * inUserData ); */
-    interface EventHandlerProcPtr extends Callback {
+    static interface EventHandlerProcPtr extends Callback {
         public OSStatus callback(Pointer inHandlerCallRef, Pointer inEvent, Pointer inUserData);
         //OSStatus callback(EventHandlerCallRef inHandlerCallRef, EventRef inEvent, Pointer inUserData); }
 
---
+++ src/main/java/com/tulskiy/keymaster/osx/Test.java
@@ -0,0 +1,76 @@
+package com.tulskiy.keymaster.osx;
+
+import com.sun.jna.Pointer;
+import com.sun.jna.ptr.PointerByReference;
+import static com.tulskiy.keymaster.osx.CarbonLib.*;
+
+/**
+ * Author: Denis Tulskiy
+ * Date: 6/16/11
+ */
+public class Test {
+    static final int kEventHotKeyPressed = 5;
+    static final int kEventHotKeyReleased = 6;
+
+    static final int kEventClassKeyboard = ((int) 'k' << 24) + ((int) 'e' << 16) + ((int) 'y' << 8) + (int) 'b';
+
+    static final int cmdKey     = 0x0100;
+    static final int shiftKey   = 0x0200;
+    static final int optionKey  = 0x0800;
+    static final int controlKey = 0x1000;
+
+    public static void main(String[] args) {
+        try
+        {
+            // Our events handlers we want to listen to
+            //EventHandlerRef		sHandler, fHandler;
+            PointerByReference fHandlerRef;
+            PointerByReference gMyHotKeyRef;
+            fHandlerRef  = new PointerByReference();
+            gMyHotKeyRef = new PointerByReference();
+
+            EventHandlerUPP myKeyListener = new EventHandlerUPP() {
+                public OSStatus callback(Pointer inHandlerCallRef, Pointer inEvent, Pointer inUserData) {
+                    return noErr;
+                }
+            };
+
+            // use an event that isn't monitored just so we have a valid EventTypeSpec to install
+            //EventTypeSpec	kEvents[] = { { kEventClassCommand, kEventCommandUpdateStatus } };
+            CarbonLib.EventTypeSpec[] eventTypes = (CarbonLib.EventTypeSpec[]) (new CarbonLib.EventTypeSpec().toArray(1));
+            eventTypes[0].eventClass = kEventClassKeyboard;
+            eventTypes[0].eventKind = kEventHotKeyPressed;
+
+            // install an event handler to detect clicks on the main window to tell the application which
+            // events to track/stop tracking
+            //InstallApplicationEventHandler( CmdHandler, GetEventTypeCount( kEvents ), kEvents, 0, &fHandler );
+            CarbonLib.OSStatus status = InstallEventHandler(GetApplicationEventTarget(), myKeyListener, new CarbonLib.ItemCount(1), eventTypes, null, fHandlerRef); //fHandlerRef
+
+            System.out.println("InstallEventHandler: "+status);
+
+            System.out.println(fHandlerRef.getValue()==null?"2. NULL":"2. Not NULL");
+            System.out.println(fHandlerRef.getValue());
+
+            EventHotKeyID gMyHotKeyID = new EventHotKeyID();
+            gMyHotKeyID.id = 1;
+            gMyHotKeyID.signature = ((int)'h'<< 24) +((int)'t' << 16) +((int)'k' << 8) +'1';
+
+
+            // extern OSStatus RegisterEventHotKey(UInt32 inHotKeyCode, UInt32 inHotKeyModifiers, EventHotKeyID inHotKeyID,
+            //                 EventTargetRef inTarget, OptionBits inOptions, EventHotKeyRef *  outRef)
+            // HotKey = cmdKey+controlKey+SPACE
+            status = RegisterEventHotKey(49, cmdKey+controlKey, gMyHotKeyID, GetApplicationEventTarget(), 0, gMyHotKeyRef);
+
+            System.out.println("RegisterHotKey: "+status);
+            System.out.println(gMyHotKeyRef.getValue());
+
+            System.out.println("Event handers installed");
+
+        }
+        catch (Exception e)
+        {
+            System.err.println(e);
+            e.printStackTrace();
+        }
+    }
+}
